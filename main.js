4e19767ff02810186248c0b0c33d9347bba46822
Jarttu <167183980+Jarttu@users.noreply.github.com>
2024-10-31 10:41:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function muunna(event){
+
+    event.preventdefault();
+
+    const tulos = document.getElementById('tulos');
+    const lampo = document.getElementById('hakukentta').value;
+    const muunnostyyppi = document.getElementById('muunnostyyppi').value;
+
+
+
+
+    
+}
